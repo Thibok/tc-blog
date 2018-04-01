1d44b1bf3bdbbbbfab7dc89232efff1edd5cd84a
Kriyu <thcavailles@laposte.net>
2018-04-01 23:25:26 +0200

    Create action list news, update NewsController, list.html to list.twig and add route and config to routes.xml and app.xml

--- App/Controllers/NewsController.php
+++ App/Controllers/NewsController.php
@@ -18,5 +18,15 @@ class NewsController extends Controller
 		$listNews = $manager->getList(0, $totalNews);
 
 		$this->response->render('index.twig', ['title' => 'Tc-blog', 'listNews' => $listNews]);
-    }
+	}
+	
+	public function executeList(Request $request)
+	{
+		$totalNews = $this->config->get('total_news_list_page');
+		$manager = new NewsManager;
+		
+		$listNews = $manager->getList(0, $totalNews);
+		
+		$this->response->render('list.twig', ['title' => 'Toutes les news', 'listNews' => $listNews]);
+	}
 }
